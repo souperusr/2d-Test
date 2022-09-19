f85b5dcaca0b426f395a7c700d578a53c93f806b
AndronicusRano <94577996+AndronicusRano@users.noreply.github.com>
2022-09-19 10:09:56 -0700

    fight WIP

---
+++ Assets/Stats.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stats : MonoBehaviour
+{
+    public int HP = 50;
+    public int MP = 20;
+    public int Attack = 8;
+    public int Strength = 6;
+    public int Mobility = 4;
+    public int Magic = 6;
+    String[] strArray = new String[3]; strArray[1];
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
